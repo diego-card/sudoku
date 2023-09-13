9297fbfcca075f9f1d669b31ea36e1c000707bc3
diego <diegobrgr@gmail.com>
2023-09-13 16:53:17 -0300

    Algumas mudanças e correções para deploy

--- sudoku.js
+++ sudoku.js
@@ -1,10 +1,10 @@
 //Criando a tabela 9x9
 let sudoku = [];
 
-for(let i=0;i<=8;i++){
+for (let i = 0; i <= 8; i++) {
     sudoku.push([]);
-    for(let j=0;j<=8;j++){
-            sudoku[i].push('');
+    for (let j = 0; j <= 8; j++) {
+        sudoku[i].push('');
     };
 };
 
@@ -17,34 +17,103 @@ for(let i=0;i<=8;i++){
         }
     };
 };*/
-sudoku[0][0] = '5';
-sudoku[1][0] = '7';
-sudoku[3][5] = '9';
-sudoku[2][1] = '2';
-sudoku[5][4] = '2';
-sudoku[6][1] = '3';
-sudoku[7][6] = '7';
-sudoku[1][6] = '2';
-sudoku[2][8] = '8';
+// sudoku[0][0] = '5';
+// sudoku[1][0] = '7';
+// sudoku[3][5] = '9';
+// sudoku[2][1] = '2';
+// sudoku[5][4] = '2';
+// sudoku[6][1] = '3';
+// sudoku[7][6] = '7';
+// sudoku[1][6] = '2';
+// sudoku[2][8] = '8';
 
+
+const puzzles = {
+    easy: [
+        ['5', '', '', '', '7', '', '3', '', ''],
+        ['', '7', '', '', '', '2', '', '4', ''],
+        ['', '', '', '', '', '8', '6', '9', '5'],
+        ['', '', '', '8', '', '9', '', '5', '2'],
+        ['', '', '5', '', '', '', '', '', ''],
+        ['2', '9', '', '4', '', '', '8', '6', ''],
+        ['9', '', '3', '', '6', '', '', '', ''],
+        ['', '', '4', '', '8', '', '', '7', ''],
+        ['', '', '', '3', '', '', '', '', '1']
+    ],
+    medium: [
+        ['', '', '2', '', '', '', '7', '9', '8'],
+        ['', '', '', '', '9', '', '', '', ''],
+        ['4', '7', '', '', '', '', '1', '', ''],
+        ['', '', '', '', '', '2', '', '5', ''],
+        ['', '1', '', '', '', '', '', '', ''],
+        ['7', '', '8', '', '', '', '', '', ''],
+        ['6', '', '', '7', '', '8', '', '', ''],
+        ['2', '', '', '9', '', '', '', '', ''],
+        ['', '', '', '', '', '5', '3', '1', '']
+    ],
+    hard: [
+        ['8', '', '', '', '', '5', '', '', ''],
+        ['', '', '6', '9', '', '', '', '', ''],
+        ['', '', '', '', '', '', '1', '8', ''],
+        ['', '5', '', '', '', '', '4', '', ''],
+        ['', '', '', '6', '', '8', '', '', ''],
+        ['', '', '', '', '', '', '', '', ''],
+        ['', '', '', '3', '', '1', '', '', ''],
+        ['', '1', '4', '', '', '', '', '', ''],
+        ['', '', '7', '', '', '', '6', '3', '']
+    ]
+}
+
+function populateSudokuGrid(difficulty) {
+        if (difficulty in puzzles) {
+    sudoku = JSON.parse(JSON.stringify(puzzles[difficulty])); // Clone the puzzle
+    updateSudokuGrid(); // Update the HTML grid
+} else {
+    console.log("Invalid difficulty level. Please choose 'easy', 'medium', or 'hard'.");
+}
+}
+
+// Function to update the HTML Sudoku grid
+function updateSudokuGrid() {
+    for (let col = 0; col <= 8; col++) {
+        for (let row = 0; row <= 8; row++) {
+            const input = $(`#c${col}${row}`);
+            input.val(sudoku[col][row]);
+            input.css({ "color": "black" });
+        }
+    }
+}
+
+// Add click event listeners to the buttons
+$(document).ready(function () {
+    $("#easyButton").click(function () {
+        populateSudokuGrid("easy");
+    });
+    $("#mediumButton").click(function () {
+        populateSudokuGrid("medium");
+    });
+    $("#hardButton").click(function () {
+        populateSudokuGrid("hard");
+    });
+});
 
 //Deixando a tabela interativa com o HTML, usando JQuery
-for(let col=0; col <= 8;col++){
-    
-    let tr = $('<tr>');
-    for (let row=0; row <= 8;row++) {
-            //Para completar o sudoku execute a função abaixo
-            //sodokoSolver(sudoku);
-            jogoVencido(sudoku)
-            let input = $(`<input type="text" id="c${col}${row}" value="${sudoku[col][row]}">`)
-            let td = $(`<td>`);
-            td.append(input);
-            tr.append(td);
+for (let col = 0; col <= 8; col++) {
 
-            //Quando alterar o input, aplique a função de alterarInput
-            input.on('input', alterarInput)
-            
-        };
+    let tr = $('<tr>');
+    for (let row = 0; row <= 8; row++) {
+        //Para completar o sudoku execute a função abaixo
+        // sodokoSolver(sudoku);
+        jogoVencido(sudoku);
+        let input = $(`<input type="text" id="c${col}${row}" value="${sudoku[col][row]}">`)
+        let td = $(`<td>`);
+        td.append(input);
+        tr.append(td);
+
+        //Quando alterar o input, aplique a função de alterarInput
+        input.on('input', alterarInput)
+
+    };
     $('#grade').append(tr);
 }
 //Função de edição de elementos que reflete no Array
@@ -55,30 +124,31 @@ function alterarInput() {
     let valor = el.val();
 
     if (validPlay(sudoku, _col, _row, valor) || $(this).val() == "") {
-        $(this).css({"color": "black"});
+        $(this).css({ "color": "black" });
         sudoku[_col][_row] = valor;
     } else {
-        $(this).css({"color": "red"});
+        $(this).css({ "color": "red" });
         console.log("errado");
     };
 
-    console.log(sudoku);
+    // console.log(sudoku);
 
     if (jogoVencido(sudoku)) {
-        console.log("você venceu");
+        alert("Parabéns, você venceu!");
+        // console.log("você venceu");
     }
 }
 
 function validPlay(tabela, col, row, value) {
 
-    for(let i = 0; i <= 8; i++){
+    for (let i = 0; i <= 8; i++) {
 
         //Verificando se o número existe e se ele é entre 1 a 9
-        if (!(value && value > 0 && value < 10)){
+        if (!(value && value > 0 && value < 10)) {
             return false;
         };
         //Verificando se é único no row/linha
-        if (value == tabela[col][0] || value == tabela[col][i+1]) {
+        if (value == tabela[col][0] || value == tabela[col][i + 1]) {
             return false;
         };
         //Verificando se é único na column/coluna
@@ -97,42 +167,42 @@ function validPlay(tabela, col, row, value) {
     };
 
     return true;
-    
+
 };
 
 //Resolvedor de Sudoku automático
 
 function sodokoSolver(data) {
     for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        if (data[i][j] == '') {
-          for (let k = 1; k <= 9; k++) {
-            if (validPlay(data, i, j, k)) {
-              data[i][j] = `${k}`;
-            if (sodokoSolver(data)) {
-             return true;
-            } else {
-             data[i][j] = '';
+        for (let j = 0; j < 9; j++) {
+            if (data[i][j] == '') {
+                for (let k = 1; k <= 9; k++) {
+                    if (validPlay(data, i, j, k)) {
+                        data[i][j] = `${k}`;
+                        if (sodokoSolver(data)) {
+                            return true;
+                        } else {
+                            data[i][j] = '';
+                        }
+                    }
+                }
+                return false;
             }
-           }
-         }
-         return false;
-       }
+        }
     }
-   }
-   return true;
-  }
+    return true;
+}
 
 //Verificação de jogo acabou
 function jogoVencido(tabela) {
-    for(let i=0;i<=8;i++){
-        for(let j=0;j<=8;j++){
+    for (let i = 0; i <= 8; i++) {
+        for (let j = 0; j <= 8; j++) {
             if (tabela[i][j] == '') {
                 return false;
             }
         }
     }
-    console.log("você venceu")
+    console.log("você venceu");
     return true;
 }
 
